5d328e0ee58cf7dde96a9a4040d63ab4895e203b
WinStarry <xuechao.yang@foxmail.com>
2020-10-10 16:01:36 +0800

    update data

--- javaSE/01-java基础语法/src/com/winstarry/demo01/HelloWorld.java
+++ javaSE/01-java基础语法/src/com/winstarry/demo01/HelloWorld.java
@@ -6,5 +6,7 @@ java程序的基础入门
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("hello,java!");
+
     }
 }
